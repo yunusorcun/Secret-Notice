117b02a4c10aac8e14961ec3d11263ab4420cf8c
yunusorcun <86880112+yunusorcun@users.noreply.github.com>
2025-02-07 16:27:46 +0300

    second phase added

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from PIL import Image, ImageTk
 
 # Ekran oluşturma
 window = tk.Tk()
+FONT=("Verdana",20,"normal")
 window.title("Secret Notice")
 window.minsize(width=350, height=600)
-window.config(padx=15, pady=15)
+window.config(padx=10, pady=10)
 window.config(bg="black")
+
 
 # Görüntü dosyasının yolu
 image_path = "E:/Pyton Öğreniyorum/Pycharm/pythonProject/Secret_Notice/arma.jpg"
@@ -24,8 +26,15 @@ try:
 except Exception as e:
     print(f"Error: {e}")
 
+#butona atama yapma
+
+def Save_Encrypt():
+    title = title_input.get()
+
+
+
 #Başlık oluşturma alanı oluşturmak için
-title_input_label= Label(text="Enter your title")
+title_input_label= Label(text="Enter your title",font=FONT)
 title_input_label.config(padx=10,pady=10)
 title_input_label.config(bg="black",fg="white")
 title_input_label.pack()
@@ -37,7 +46,7 @@ title_input.pack()
 
 #Saklamak istedğin bilgiyi gir.
 secrets_input_label= Label(text="Enter Your Secret")
-secrets_input_label.config(padx=10,pady=15)
+secrets_input_label.config(padx=10,pady=10)
 secrets_input_label.config(bg="black",fg="white")
 secrets_input_label.pack()
 
@@ -46,7 +55,7 @@ secrets_text.pack()
 
 #Bilgi almak için key girmemiz gerekiyor bu kısım onun için.
 key_input_label= Label(text="Enter Master Key")
-key_input_label.config(padx=10,pady=15)
+key_input_label.config(padx=10,pady=10)
 key_input_label.config(bg="black",fg="white")
 key_input_label.pack()
 
@@ -54,10 +63,13 @@ title_input = Entry(width=30, bg="white")
 title_input.focus()
 title_input.pack()
 
-#Girdiğimiz verileri karşılaştırmak için buton eklemeliyiz.
+#save button
+save_button=Button(text="Save & Encrypt",command=Save_Encrypt)
+save_button.pack()
 
-#save_button=Button("Save and Encrypt") #command kısmı gir
-#save_button.pack()
+#Girdiğimiz verileri karşılaştırmak için buton eklemeliyiz.
+decrypt_button=Button(text="decrypt") #command kısmı gir
+decrypt_button.pack()
 
 
 
