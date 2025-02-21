e51168af7362e96befaa982c9f9ec37039f8a80f
yunusorcun <86880112+yunusorcun@users.noreply.github.com>
2025-02-21 21:33:26 +0300

    encrypt code added

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import messagebox
+import base64
+def encode(key,clear):
+    enc=[]
+    for i in range(len(clear)):
+        key_c= key[i%len(key)]
+        enc_c=chr((ord(clear[i])+ord(key_c))%256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+def decode(key,enc):
+    dec=[]
+    enc= base64.urlsafe_b64encode(enc).decode()
+    for i in range(len(enc)):
+        key_c=key[i%len(key)]
+        dec_c= chr((256+ord(edc[i])-ord(key_c))%256)
+        dec.append(dec_c)
+    return "".join(dec)
 
 def save_and_encrypt_notes():
     title = title_entry.get()
@@ -10,8 +27,10 @@ def save_and_encrypt_notes():
     if len(title) == 0 or len(message) == 0 or len(master_secret) == 0:
         messagebox.showinfo(title="Error", message="Please enter all info")
     else:
+        #encryption
+        massage_encrypted=encode(master_secret,message)
         with open("mysecret.txt", "a") as data_file:  # Append modunda açıyoruz.
-            data_file.write(f"\n{title}\n{message}")  # Python string de yeni satıra geçme.
+            data_file.write(f"\n{title}\n{massage_encrypted}")  # Python string de yeni satıra geçme.
         title_entry.delete(0, END) #mysecret i silmek için.
         secrets_text.delete("1.0", END)
         master_secrets_entry.delete(0, END)
